fdd04acb15ce2d2c07ade3f61a0783b7dbd1e618
AgathaS <farinha1960@outlook.com>
2025-10-17 16:52:19 +0200

    Initial commit - Spring Boot Factory API

---
+++ src/main/java/com/ApiFactory/ApiFactory/ApiFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.ApiFactory.ApiFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApiFactory/ApiFactory/ApiFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApiFactory.ApiFactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
