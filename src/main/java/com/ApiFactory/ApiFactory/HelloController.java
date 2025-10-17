b6f420316271da226bda31b814209adbbf5ee4f9
AgathaS <farinha1960@outlook.com>
2025-10-17 17:25:40 +0200

    fisrt hello test and enable JPA by application yml

---
+++ src/main/java/com/ApiFactory/ApiFactory/HelloController.java
@@ -0,0 +1,12 @@
+package com.ApiFactory.ApiFactory;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+     @GetMapping("/hello")
+    public String hello() {
+        return "Hello Philiiiiiiiippe!";
+    }
+}
