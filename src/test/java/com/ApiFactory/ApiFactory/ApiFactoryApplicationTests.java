56d93e559e218160e5b63f768052884da20c41b3
AgathaS <farinha1960@outlook.com>
2025-10-20 15:46:32 +0200

    persistance : h2 memory by file update + add application-test.yml

--- src/test/java/com/ApiFactory/ApiFactory/ApiFactoryApplicationTests.java
+++ src/test/java/com/ApiFactory/ApiFactory/ApiFactoryApplicationTests.java
@@ -25,6 +25,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -35,6 +36,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ApiFactoryApplicationTests {
 
